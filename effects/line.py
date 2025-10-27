063be806296a7f8cbbdbfef39e44c0eab343e7fc
dilirink <dilirink@gmail.com>
2025-10-28 02:03:28 +0300

    new

--- effects/line.py
+++ effects/line.py
@@ -2,7 +2,13 @@
 import time
 import random
 import math
-from RGBMatrixEmulator import RGBMatrix, RGBMatrixOptions
+import platform
+
+if platform.system() == "Windows":
+    from RGBMatrixEmulator import RGBMatrix, RGBMatrixOptions
+    graphics = None  # если нужно
+else:
+    from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 from PIL import Image, ImageDraw
 
 # ============ НАСТРОЙКИ МАТРИЦЫ ============
--- effects/sorting_algoritm.py
+++ effects/sorting_algoritm.py
@@ -6,7 +6,13 @@
 
 import time
 import random
-from RGBMatrixEmulator import RGBMatrix, RGBMatrixOptions
+import platform
+
+if platform.system() == "Windows":
+    from RGBMatrixEmulator import RGBMatrix, RGBMatrixOptions
+    graphics = None  # если нужно
+else:
+    from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 from PIL import Image, ImageDraw, ImageFont
 
 # ========== НАСТРОЙКИ ==========
